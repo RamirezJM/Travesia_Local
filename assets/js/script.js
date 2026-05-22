2d144d7a2c57336caa208e343fe6b3c70992bef3
RamirezJM <165516814+RamirezJM@users.noreply.github.com>
2026-05-21 22:58:26 -0400

    feat: agregar respuesta modal en inglés

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ const modalRespuesta = new bootstrap.Modal(document.querySelector('#modal-respue
 const respuestaTitulo = document.querySelector('#modal-respuesta-titulo')
 const respuestaImagen = document.querySelector('#modal-respuesta-imagen')
 const respuestaMensaje = document.querySelector('#modal-respuesta-mensaje')
+const lenguaje = document.documentElement.lang || 'es';
 
 
 form.addEventListener('submit', async (ev) => {
@@ -27,19 +28,48 @@ form.addEventListener('submit', async (ev) => {
     });
 
     if (response.ok) {
-      respuestaTitulo.innerText = '¡Mensaje Enviado!'
-      respuestaImagen.src = 'assets/images/iconos/respuesta-ok.svg'
-      respuestaMensaje.innerText = 'Gracias por contactarnos. Nos comunicaremos contigo a la brevedad.'
+      if (lenguaje === 'es') {
+        respuestaTitulo.innerText = '¡Mensaje Enviado!'
+        respuestaImagen.src = 'assets/images/iconos/respuesta-ok.svg'
+        respuestaMensaje.innerText = 'Gracias por contactarnos. Nos comunicaremos contigo a la brevedad.'
+      }
+      else {
+        respuestaTitulo.innerText = '¡Message Sent!'
+        respuestaImagen.src = 'assets/images/iconos/respuesta-ok.svg'
+        respuestaMensaje.innerText = 'Thank you for reaching out. We will get back to you as soon as possible.'
+      }
       form.classList.remove('was-validated');
       form.reset();
     } else {
-      respuestaTitulo.innerText = 'Ha ocurrido un error'
-      respuestaImagen.src = 'assets/images/iconos/respuesta-error.svg'
-      respuestaMensaje.innerText = 'No pudimos procesar tu solicitud. Por favor intenta nuevamente o contáctanos a través de nuestras redes sociales.'
-      const responseData = await response.json();
-      console.log(responseData);
-      form.reset();
+      if (lenguaje === 'es') {
+        respuestaTitulo.innerText = 'Ha ocurrido un error'
+        respuestaImagen.src = 'assets/images/iconos/respuesta-error.svg'
+        respuestaMensaje.innerText = 'No pudimos procesar tu solicitud. Por favor intenta nuevamente o contáctanos a través de nuestras redes sociales.'
+      } else {
+        respuestaTitulo.innerText = 'Something went wrong'
+        respuestaImagen.src = 'assets/images/iconos/respuesta-error.svg'
+        respuestaMensaje.innerText = "We couldn't send your message. Please try again or contact us through our social media platforms."
+      }
+       const responseData = await response.json();
+       form.reset();
     }
+
+
+    /* 
+        if (response.ok) {
+          respuestaTitulo.innerText = '¡Mensaje Enviado!'
+          respuestaImagen.src = 'assets/images/iconos/respuesta-ok.svg'
+          respuestaMensaje.innerText = 'Gracias por contactarnos. Nos comunicaremos contigo a la brevedad.'
+          form.classList.remove('was-validated');
+          form.reset();
+        } else {
+          respuestaTitulo.innerText = 'Ha ocurrido un error'
+          respuestaImagen.src = 'assets/images/iconos/respuesta-error.svg'
+          respuestaMensaje.innerText = 'No pudimos procesar tu solicitud. Por favor intenta nuevamente o contáctanos a través de nuestras redes sociales.'
+          const responseData = await response.json();
+          console.log(responseData);
+          form.reset();
+        } */
 
   }
   modalRespuesta.show()
